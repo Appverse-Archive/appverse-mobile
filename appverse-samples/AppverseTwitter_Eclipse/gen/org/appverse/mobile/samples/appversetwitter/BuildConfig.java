7a26debf2fb4ecf3b50bca30d39670c93e27cdef
marguiline <maps@unityversal.com>
2012-12-28 10:16:38 +0100

    Added sample Android project for Eclipse
    A new version of the AppverseTwitter application for building in
    Eclipse and ADT Plugin

---
+++ appverse-samples/AppverseTwitter_Eclipse/gen/org/appverse/mobile/samples/appversetwitter/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package org.appverse.mobile.samples.appversetwitter;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
