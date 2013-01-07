5ad20571c4ff8b710ab8950bed640c6f3717503b
Marga Parets Luque <maps@unityversal.com>
2013-01-07 10:31:33 +0100

    fixed wrong characters in Appverse license text

--- appverse-platform-android/src/com/gft/unity/android/AndroidUtils.java
+++ appverse-platform-android/src/com/gft/unity/android/AndroidUtils.java
@@ -2,7 +2,7 @@
  Copyright (c) 2012 GFT Appverse, S.L., Sociedad Unipersonal.
 
  This Source  Code Form  is subject to the  terms of  the Appverse Public License 
- Version 2.0  (ÒAPL v2.0Ó).  If a copy of  the APL  was not  distributed with this 
+ Version 2.0  ("APL v2.0").  If a copy of  the APL  was not  distributed with this 
  file, You can obtain one at http://appverse.org/legal/appverse-license/.
 
  Redistribution and use in  source and binary forms, with or without modification, 
