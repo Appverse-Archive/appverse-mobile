702668e189c5232257f91968e0675f76c62606a2
David Barranco <theberserk@gmail.com>
2013-12-24 13:59:41 +0100

    Added Keychain Native tests

---
+++ appverse-samples/NativeTests/iOSNativeTest/testNAtiveKeychain/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testNAtiveKeychain
+//
+//  Created by David Barranco on 9/3/13.
+//  Copyright (c) 2013 David Barranco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
