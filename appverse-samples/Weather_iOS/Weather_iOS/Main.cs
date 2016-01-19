6422ad06b0791da62456ff75274cdc5c3f59ede9
Francisco Javier Martin Bueno <franciscojaviermartinb@gmail.com>
2016-01-19 16:37:28 +0100

    iosSample

---
+++ appverse-samples/Weather_iOS/Weather_iOS/Main.cs
@@ -0,0 +1,86 @@
+/*
+ Copyright (c) 2012 GFT Appverse, S.L., Sociedad Unipersonal.
+
+ This Source  Code Form  is subject to the  terms of  the Appverse Public License 
+ Version 2.0  (“APL v2.0”).  If a copy of  the APL  was not  distributed with this 
+ file, You can obtain one at http://appverse.org/legal/appverse-license/.
+
+ Redistribution and use in  source and binary forms, with or without modification, 
+ are permitted provided that the  conditions  of the  AppVerse Public License v2.0 
+ are met.
+
+ THIS SOFTWARE IS PROVIDED BY THE  COPYRIGHT HOLDERS  AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS  OR IMPLIED WARRANTIES, INCLUDING, BUT  NOT LIMITED TO,   THE IMPLIED
+ WARRANTIES   OF  MERCHANTABILITY   AND   FITNESS   FOR A PARTICULAR  PURPOSE  ARE
+ DISCLAIMED. EXCEPT IN CASE OF WILLFUL MISCONDUCT OR GROSS NEGLIGENCE, IN NO EVENT
+ SHALL THE  COPYRIGHT OWNER  OR  CONTRIBUTORS  BE LIABLE FOR ANY DIRECT, INDIRECT,
+ INCIDENTAL,  SPECIAL,   EXEMPLARY,  OR CONSEQUENTIAL DAMAGES  (INCLUDING, BUT NOT
+ LIMITED TO,  PROCUREMENT OF SUBSTITUTE  GOODS OR SERVICES;  LOSS OF USE, DATA, OR
+ PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) 
+ ARISING  IN  ANY WAY OUT  OF THE USE  OF THIS  SOFTWARE,  EVEN  IF ADVISED OF THE 
+ POSSIBILITY OF SUCH DAMAGE.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+using Unity.Platform.IPhone;
+
+namespace UnityUI.iOS
+{
+	public class Application
+	{
+		// This is the main entry point of the application.
+		static void Main (string[] args)
+		{
+			try  {
+				#if DEBUG
+					Console.WriteLine("Starting main application...");
+				#endif
+
+				// if you want to use a different Application Delegate class from "AppDelegate"
+				// you can specify it here.
+
+
+				DeviceHardware.HardwareVersion hwVersion = DeviceHardware.Version;
+
+				#if DEBUG
+				Console.WriteLine("Current Device Version: " + hwVersion);
+				#endif
+			/* TO BE REMOVED - 5.0.6 [AMOB-30]
+
+				if (UIDevice.CurrentDevice.CheckSystemVersion (8, 0)) {
+
+					// Previous issues using WKWebview in 64-bits devices were solved compiling for both 32 and 64 bits
+
+					#if DEBUG
+					Console.WriteLine("Loading Application Delegate (support for iOS8)");
+					#endif
+					UIApplication.Main (args, null, "AppDelegate_WKWebView");
+
+				} else {
+			*/
+					#if DEBUG
+					Console.WriteLine("Loading Application Delegate");
+					#endif
+					UIApplication.Main (args, null, "AppDelegate_UIWebView");
+			//	}
+				
+				#if DEBUG
+					Console.WriteLine("END main application");
+				#endif
+				
+			} catch (Exception ex) {
+				#if DEBUG
+					Console.WriteLine("Exception on MAIN Application method: " + ex.Message);
+					Console.WriteLine("Stacktrace ---------------------");
+					Console.WriteLine(ex.StackTrace);
+				#endif
+			}
+		}
+	}
+}
